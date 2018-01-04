44c21bdf5d0f4cc1d80c372ebaa6fab3483a5bff
EthanMHC <echung2011@gmail.com>
2018-01-03 20:45:08 -0800

    Week 2 Revisions

--- ch08/arraySort.rb
+++ ch08/arraySort.rb
@@ -2,13 +2,13 @@ puts "Please begin by adding as many words as you wish to the array."
 puts "When you are done, press ENTER."
 array = []
 input = gets.chomp
-	if input != ""
-		while input != ""
-			array.push input 
-		input = gets.chomp
-			if input == ""
-			break
-		end
-	end
+if input != ""
+  while input != ""
+	array.push input 
+	input = gets.chomp
+    if input == ""
+      break
+    end
+  end
 end
 puts array.sort
